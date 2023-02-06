28a2e9ca2199f8f079c3ac5614c3370583485953
daviromao <davi.romao0210@gmail.com>
2023-02-05 22:26:54 -0300

    style: change to singlequote

---
+++ packages/server/src/user/userType.ts
@@ -0,0 +1,21 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
+import { globalIdField } from "graphql-relay";
+import { User } from "./userModel";
+
+export const UserType = new GraphQLObjectType<User>({
+  name: "User",
+  fields: () => ({
+    id: globalIdField("User"),
+    name: {
+      type: GraphQLString,
+      resolve: (user) => user.name,
+    },
+    phone: {
+      type: GraphQLString,
+      resolve: (user) => user.phone,
+    },
+  }),
+});
