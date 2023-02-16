b431bf7b1d2854f6e145da87904966ff0994e060
daviromao <davi.romao0210@gmail.com>
2023-02-16 18:50:18 -0300

    feat: create validators for createGiveawayMutation

--- packages/server/src/giveaway/mutations/createGiveawayMutation.ts
+++ packages/server/src/giveaway/mutations/createGiveawayMutation.ts
@@ -2,6 +2,23 @@ import { GraphQLInt, GraphQLString } from 'graphql';
 import { mutationWithClientMutationId } from 'graphql-relay';
 import { createGiveaway } from '../giveawayService';
 import { GiveawayType } from '../giveawayType';
+import * as Yup from 'yup';
+
+type CreateGiveawayArgs = {
+  title: string;
+  description: string;
+  winnersCount: number;
+  endsAt: string | Date;
+};
+
+const CreateGiveawaySchema = Yup.object().shape({
+  title: Yup.string().required().min(3).max(100),
+  description: Yup.string().required().min(3).max(1000),
+  winnersCount: Yup.number().required().min(1).max(100),
+  endsAt: Yup.date()
+    .required()
+    .min(new Date(), 'Giveaway must end in the future'),
+});
 
 export const CreateGiveawayMutation = mutationWithClientMutationId({
   name: 'CreateGiveaway',
@@ -19,19 +36,26 @@ export const CreateGiveawayMutation = mutationWithClientMutationId({
     },
   },
   async mutateAndGetPayload(
-    { title, description, winnersCount, endsAt },
+    { title, description, winnersCount, endsAt }: CreateGiveawayArgs,
     context
   ) {
     if (!context.state.user) {
       throw new Error('You must be logged in to create a giveaway');
     }
 
+    await CreateGiveawaySchema.validate({
+      title,
+      description,
+      winnersCount,
+      endsAt,
+    });
+
     const giveaway = await createGiveaway({
       title,
       description,
       winnersCount,
       owner: context.state.user._id,
-      endsAt,
+      endsAt: endsAt as any,
     });
 
     return giveaway;
--- packages/server/src/giveaway/mutations/joinInGiveawayMutation.ts
+++ packages/server/src/giveaway/mutations/joinInGiveawayMutation.ts
@@ -1,6 +1,15 @@
 import { GraphQLBoolean, GraphQLString } from 'graphql';
 import { fromGlobalId, mutationWithClientMutationId } from 'graphql-relay';
 import { insertParticipantInGiveaway } from '../giveawayService';
+import * as Yup from 'yup';
+
+type JoinInGiveawayArgs = {
+  giveawayId: string;
+};
+
+const JoinInGiveawaySchema = Yup.object().shape({
+  giveawayId: Yup.string().required(),
+});
 
 export const JoinInGiveaway = mutationWithClientMutationId({
   name: 'JoinInGiveaway',
@@ -13,10 +22,14 @@ export const JoinInGiveaway = mutationWithClientMutationId({
       type: GraphQLBoolean,
     },
   },
-  async mutateAndGetPayload({ giveawayId }, context) {
+  async mutateAndGetPayload({ giveawayId }: JoinInGiveawayArgs, context) {
     if (!context.state.user) {
       throw new Error('You must be logged in to join in a giveaway');
     }
+
+    await JoinInGiveawaySchema.validate({
+      giveawayId,
+    });
 
     const joinInAGiveaway = await insertParticipantInGiveaway(
       fromGlobalId(giveawayId).id,
